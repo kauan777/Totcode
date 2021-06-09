fba247e9199a7b3a77db74f4ba5197433b65a0f9
kauan costa <kauan7622@gmail.com>
2021-06-09 20:02:47 -0300

    portifolio v1

--- src/script.js
+++ src/script.js
@@ -14,3 +14,46 @@ btnmenu.addEventListener("click", ()=>{
     nav.classList.toggle("style");
     btnmenu.classList.toggle("X")
 })
+
+
+let angle = 0;
+// let i = 0;
+// const img = document.querySelectorAll("img.img-slide")
+
+
+function galleryspinright(sign){
+   
+    spinner = document.querySelector(".spinner");
+    if(!sign){
+        angle = angle + 45;
+        
+    }else{
+        angle = angle - 45;
+    }
+    spinner.setAttribute("style", "transform: rotateY("+angle+"deg);")
+}
+
+
+// function addFront(){
+//     img[i].classList.remove("selected")
+//     i++
+//     img[i].classList.add("selected")
+// }
+
+function galleryspinleft(sign1){
+    spinner = document.querySelector(".spinner");
+    if(!sign1){
+        angle = angle - 45;
+    }else{
+        angle = angle + 45;
+    }
+    spinner.setAttribute("style", "transform: rotateY("+angle+"deg);")
+}
+
+
+
+
+
+
+
+
